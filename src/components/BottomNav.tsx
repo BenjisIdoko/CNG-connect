80f442ec7d37f58d9c651c03c13628ab063e59f2
benjamin <strictly4eternity@gmail.com>
2026-08-22 22:56:43 +0100

    style(nav): set conversion center icon to handyman in BottomNav

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -49,7 +49,7 @@ export const BottomNav: React.FC<BottomNavProps> = ({
             activeTab === 'conversions' ? 'material-symbols-fill' : ''
           }`}
         >
-          wrench
+          handyman
         </span>
       </button>
 
