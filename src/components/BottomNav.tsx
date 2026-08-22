62baca4b850b6466d885659d67e6a55440a44593
benjamin <strictly4eternity@gmail.com>
2026-08-22 23:02:10 +0100

    style(nav): update conversion center icon to propane_tank in BottomNav

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -49,7 +49,7 @@ export const BottomNav: React.FC<BottomNavProps> = ({
             activeTab === 'conversions' ? 'material-symbols-fill' : ''
           }`}
         >
-          handyman
+          propane_tank
         </span>
       </button>
 
